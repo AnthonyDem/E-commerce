1ebdc625b5d44b6729b8f5e80c364965e16ddffc
AnthonyDem <thony.dem@gmail.com>
2020-12-23 18:14:06 +0300

    creating respective selectors

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CollectionComponent from "../collection-component/collection.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import 'collection-overview.styles.scss';
 
 const collectionOverview = ({ collections }) => (
@@ -14,7 +14,7 @@ const collectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(collectionOverview)
--- src/pages/collection/collection-page.component.jsx
+++ src/pages/collection/collection-page.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './collection-page.styles.scss';
+import { connect } from 'redux';
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
-const CollectionPage = ({ match }) => (
-    <div className='category'>
-        <h2>CATEGORY</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const [title, items] = collection;
+    return (
+        <div className='category'>
+            <h2 className="title">{ title }</h2>
+            <div className="items">
+                { items.map(item => <CollectionItem item={item} key={item.id}/>)}
+            </div>
+        </div>
+    );
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
